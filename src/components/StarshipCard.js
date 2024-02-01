bea7c41754c671b7aa55922a7253c53b08744fcd
Mindy D Tran <mindytran98@gmail.com>
2024-02-01 14:03:59 -0500

    add link and classname

--- src/components/StarshipCard.js
+++ src/components/StarshipCard.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function StarshipCard({starship}) {
   const {name} =starship;
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="card">
+      <Link to={`/starship/${name}`} >
+        <h1>{name}</h1>
+      </Link>
     </div>
   );
 }
