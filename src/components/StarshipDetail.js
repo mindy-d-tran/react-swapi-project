03bebf1fced7b06e330669720c4630127dd9f660
Mindy D Tran <mindytran98@gmail.com>
2024-02-01 14:04:27 -0500

    boilerplate

---
+++ src/components/StarshipDetail.js
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from "react";
+
+export default function StarshipDetail() {
+//   const [starhip, setStarship] = useState(null);
+
+  return (
+    <div>
+      <h1>Hellow</h1>
+    </div>
+  );
+}
